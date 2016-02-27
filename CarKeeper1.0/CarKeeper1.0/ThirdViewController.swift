72bd7aa7559caf2ee7e4e8a6fed888d8c5a9a59d
Stephen Kwan <skwan@vols.utk.edu>
2016-02-27 18:10:36 -0500

    CarKeeper v.1.0

---
+++ CarKeeper1.0/CarKeeper1.0/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  CarKeeper1.0
+//
+//  Created by Stephen Kwan on 2/27/16.
+//  Copyright © 2016 Alto. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
