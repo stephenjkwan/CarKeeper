ef29b9b892a4b3b07be2a0648d97721a4c9366e1
Stephen Kwan <skwan@vols.utk.edu>
2016-03-15 16:04:26 -0700

    Added the add new car view

---
+++ CarKeeper1.0/CarKeeper1.0/CarKeeper1.0/Cars.swift
@@ -0,0 +1,14 @@
+//
+//  Cars.swift
+//  CarKeeper1.0
+//
+//  Created by Stephen Kwan on 3/15/16.
+//  Copyright © 2016 Alto. All rights reserved.
+//
+
+import Foundation
+
+class Car {
+    var Name: String?
+    var GasMileage: Double?
+}
---
+++ CarKeeper1.0/CarKeeper1.0/CarKeeper1.0/SaveMenu.swift
@@ -0,0 +1,26 @@
+//
+//  SaveMenu.swift
+//  CarKeeper1.0
+//
+//  Created by Stephen Kwan on 3/15/16.
+//  Copyright © 2016 Alto. All rights reserved.
+//
+
+import UIKit
+
+class SaveMenu: UIViewController {
+    
+    @IBAction func SaveCarButton(sender: AnyObject) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
