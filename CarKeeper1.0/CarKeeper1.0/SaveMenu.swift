8d675ad279e2803829f31352f96b245a1c8ce300
Stephen Kwan <skwan@vols.utk.edu>
2016-03-16 17:12:08 -0700

    Working on add memory

--- CarKeeper1.0/CarKeeper1.0/SaveMenu.swift
+++ CarKeeper1.0/CarKeeper1.0/SaveMenu.swift
@@ -9,10 +9,53 @@
 import UIKit
 
 class SaveMenu: UIViewController {
-    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var CarNameTextField: UITextField!
+    @IBOutlet weak var GasMileageTextField: UITextField!
     @IBAction func SaveCarButton(sender: AnyObject) {
-        
+        let newCar = Car()
+        newCar.GasMileage = NSNumberFormatter().numberFromString(GasMileageTextField.text!)?.doubleValue
+        newCar.Name = CarNameTextField.text
+        print("Car Name: \(newCar.Name)")
+        print("Car Mileage \(newCar.GasMileage)")
+
+        if(newCar.Name != nil){
+            print("Car Name: \(newCar.Name)")
+        }
+        else{
+            print("No Car Name")
+        }
+        if(newCar.GasMileage != nil){
+            print("Car Mileage \(newCar.GasMileage)")
+        }
+        else{
+            print("No inputted Gas Mileage")
+        }
     }
+    @IBAction func testSaveButton(sender: AnyObject)
+    {
+        let newCar = Car()
+        newCar.GasMileage = NSNumberFormatter().numberFromString(GasMileageTextField.text!)?.doubleValue
+        newCar.Name = CarNameTextField.text
+        print("Car Name: \(newCar.Name)")
+        print("Car Mileage \(newCar.GasMileage)")
+        
+        if(newCar.Name != nil){
+            print("Car Name: \(newCar.Name!)")
+        }
+        else{
+            print("No Car Name")
+        }
+        if(newCar.GasMileage != nil){
+            print("Car Mileage \(newCar.GasMileage!)")
+        }
+        else{
+            print("No inputted Gas Mileage")
+        }
+        label1.text = newCar.Name!
+        label2.text = String(format:"%f",newCar.GasMileage!)
+            }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,5 +65,6 @@ class SaveMenu: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 }
