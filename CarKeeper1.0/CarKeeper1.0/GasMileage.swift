b4b4d8b9916d24bab17c58c4c21e52f8a863db59
Justin Nguyen <justinnguyen@Justins-MacBook-Pro.local>
2016-03-27 17:14:27 -0400

    Testing/Updated Keeper

---
+++ CarKeeper1.0/CarKeeper1.0/GasMileage.swift
@@ -0,0 +1,24 @@
+//
+//  GasMileage.swift
+//  CarKeeper1.0
+//
+//  Created by Justin Nguyen on 3/24/16.
+//  Copyright © 2016 Alto. All rights reserved.
+//
+
+import UIKit
+
+class GasMileage{
+    
+    var : String
+    var CarPhoto: UIImage?
+    
+    init?(CarName: String, CarPhoto: UIImage?){
+        self.CarName = CarName
+        self.CarPhoto = CarPhoto
+        
+    }
+    
+    
+    
+}
---
+++ CarKeeper1.0/CarKeeper1.0/GasMileageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GasMileageTableViewCell.swift
+//  CarKeeper1.0
+//
+//  Created by Justin Nguyen on 3/24/16.
+//  Copyright © 2016 Alto. All rights reserved.
+//
+
+import UIKit
+
+class GasMileageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
