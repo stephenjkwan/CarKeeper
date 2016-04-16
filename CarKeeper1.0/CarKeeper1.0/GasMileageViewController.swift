e2d272b8e354042944cd73da08145a22c1397a9d
Stephen Kwan <skwan@vols.utk.edu>
2016-04-16 15:51:34 -0400

    Imported the ios Charts APIand currently making a graph

---
+++ CarKeeper1.0/CarKeeper1.0/GasMileageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GasMileageViewController.swift
+//  CarKeeper1.0
+//
+//  Created by Stephen Kwan on 4/16/16.
+//  Copyright © 2016 Alto. All rights reserved.
+//
+
+import UIKit
+import Charts
+/*Charts is an api for making nice looking charts. I downloaded the source code from https://github.com/danielgindi/Charts
+and learned how to include these source files through this stack overflow write up http://stackoverflow.com/questions/36584474/embedded-ios-charts-into-project-now-project-will-not-build-24-errors-in-ios-c .
+ Currently I am looking at this tutorial http://www.appcoda.com/ios-charts-api-tutorial/
+*/
+class GasMileageViewController: UIViewController {
+    @IBOutlet weak var barChartView: BarChartView!
+    override func viewDidLoad() {
+        barChartView.noDataText = "No Data Provided"
+    }
+    
+
+}
